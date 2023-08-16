487cbedf96e11a9a762688424b4e7e31cdf07e83
Fifachaimong <fifasisa36@gmail.com>
2023-08-16 14:30:30 +0700

    hw4-2

--- Homework/HW04/hw04-2-operator.cpp
+++ Homework/HW04/hw04-2-operator.cpp
@@ -3,8 +3,6 @@
 */
 #include <stdio.h>
 
-#include <stdio.h>
-
 int main() {
     int a = 0, b = 0;
     printf("Please enter values (a b): ");
@@ -14,14 +12,8 @@ int main() {
     printf("a + b = %d \n", a + b);
     printf("a - b = %d \n", a - b);
     printf("a * b = %d \n", a * b);
-    
-    if (b > 0) {
-        printf("a / b = %d \n", a / b);
-    } else {
-        printf("a / b = Undefined (b must be greater than 0)\n");
-    }
-    
-    printf("a %% b = %d \n", a % b);
+    printf("a / b = %d (b must be greater than 0)\n", b > 0 ? a / b : 0);
+    printf("a % b = %d \n", a % b);
     printf("++a = %d \n", ++a);
     printf("a-- = %d \n", a--);
     printf("a += 1 %d \n", a += 1);
@@ -31,15 +23,8 @@ int main() {
     printf("a *= 1 %d \n", a *= 1);
     printf("a *= b %d \n", a *= b);
     printf("a %%= 1 %d \n", a %= 1);
-    
-    if (b != 0) {
-        printf("a %%= b %d \n", a %= b);
-    } else {
-        printf("a %%= b = Undefined (b must not be zero)\n");
-    }
-    
+    printf("a %%= b %d \n", a %= b);
     printf("a && b %d \n", a && b);
     printf("a || b %d \n", a || b);
-    
     return 0;
 }
